01f6590cc64c16c643b21051097f6f9935cf07bb
Akhil Afriliawan <akhilafriliawan@gmail.com>
2021-06-06 21:40:45 +0700

    birthday commit

---
+++ index.php
@@ -0,0 +1,141 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Happy Birthday !</title>
+  <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400" rel="stylesheet">
+  <link rel="shortcut icon" type="image/png" href="img/favicon.png" />
+  <link rel="stylesheet" href="style/style.css">
+</head>
+<style>
+/* @media only screen and (max-width: 426px) {
+  .wish-hbd{max-height:50px;}
+}
+@media only screen and (max-width: 376px) {
+  .wish-hbd{max-height:20px;}
+} */
+</style>
+<body>
+<!-- <div style="display:none;"> -->
+    <audio src="song/ineedyoubaby.mp3" autoplay controls style="display:none;"></audio>
+<!-- </div> -->
+  <div class="container">
+    <div class="one">
+      <h1 class="one">Hai
+        <span id="name">Fachrina Noor Fauziah!</span>
+      </h1>
+      <p class="two" id="greetingText">It's me, Akhil Afriliawan. I really proud of you btw!</p>
+    </div>
+    <div class="three">
+      <p>Today is your birthday! :D</p>
+    </div>
+    <div class="four">
+      <div class="text-box">
+        <p class="hbd-chatbox">Semoga makin dewasa ya, makin produktif dan berkualitas! XD</p>
+        <p class="fake-btn">Send</p>
+      </div>
+    </div>
+    <div class="five">
+      <p class="idea-1">That's what I was going to do.</p>
+      <p class="idea-2">But then I stopped.</p>
+      <p class="idea-3">I realised, I wanted to do something
+        <strong>special</strong>.</p>
+      <p class="idea-4">Because,</p>
+      <p class="idea-5">You are Special
+        <span>:)</span>
+      </p>
+      <p class="idea-6">
+        <span>S</span>
+        <span>O</span>
+      </p>
+    </div>
+    <div class="six">
+      <img src="img/fachrina.jpg" alt="" style="width:50%;" class="lydia-dp" id="imagePath">
+      <img src="img/hat.svg" alt="" class="hat">
+      <div class="wish">
+        <h3 class="wish-hbd">Happy&ensp;Birthday&ensp;Fachrina&ensp;Noor&ensp;Fauziah</h3>
+        <h5 id="wishText">Alhamdulillah, thanks for everything to Allah! ;)</h5>
+      </div>
+    </div>
+    <div class="seven">
+      <div class="baloons">
+        <img src="img/ballon2.svg" alt="">
+        <img src="img/ballon1.svg" alt="">
+        <img src="img/ballon3.svg" alt="">
+        <img src="img/ballon1.svg" alt="">
+        <img src="img/ballon2.svg" alt="">
+        <img src="img/ballon3.svg" alt="">
+        <img src="img/ballon2.svg" alt="">
+        <img src="img/ballon3.svg" alt="">
+        <img src="img/ballon1.svg" alt="">
+        <img src="img/ballon2.svg" alt="">
+        <img src="img/ballon3.svg" alt="">
+        <img src="img/ballon2.svg" alt="">
+        <img src="img/ballon1.svg" alt="">
+        <img src="img/ballon3.svg" alt="">
+        <img src="img/ballon2.svg" alt="">
+        <img src="img/ballon3.svg" alt="">
+        <img src="img/ballon1.svg" alt="">
+        <img src="img/ballon2.svg" alt="">
+        <img src="img/ballon1.svg" alt="">
+        <img src="img/ballon3.svg" alt="">
+        <img src="img/ballon3.svg" alt="">
+        <img src="img/ballon1.svg" alt="">
+        <img src="img/ballon2.svg" alt="">
+        <img src="img/ballon3.svg" alt="">
+        <img src="img/ballon2.svg" alt="">
+        <img src="img/ballon1.svg" alt="">
+        <img src="img/ballon3.svg" alt="">
+        <img src="img/ballon2.svg" alt="">
+        <img src="img/ballon3.svg" alt="">
+        <img src="img/ballon1.svg" alt="">
+        <img src="img/ballon2.svg" alt="">
+        <img src="img/ballon1.svg" alt="">
+        <img src="img/ballon3.svg" alt="">
+      </div>
+    </div>
+    <div class="eight">
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+      <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="20" cy="20" r="20" />
+      </svg>
+    </div>
+    <div class="nine">
+      <p>I just, wanna tell you that i think you're amazing..</p>
+      <p id="replay">Click, if you want to watch it again btw</p>
+      <p class="last-smile">:)</p>
+    </div>
+  </div>
+</body>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"></script>
+<script type="application/javascript" src="script/main.js"></script>
+
+</html>
